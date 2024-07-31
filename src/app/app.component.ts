83da9cf6ec476612b67eaf5c130e42b782457585
kishan.infiadd <kishan.infiadd@gmail.com>
2024-07-31 16:33:31 +0530

    updated overview design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { SideNavbarComponent } from './shared/components/side-navbar/side-navbar.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    SideNavbarComponent,
+    NavbarComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'invoice-logic';
+export class AppComponent implements OnInit {
+  hideSidebarAndNavbar: boolean = false;
+  noPadding: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const currentRoute = this.router.url;
+        this.hideSidebarAndNavbar =
+          currentRoute === '/login' || currentRoute === '/signup';
+        this.noPadding =
+          currentRoute === '/login' || currentRoute === '/signup';
+      });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './modules/auth/login/login.component';
 import { SignupComponent } from './modules/auth/signup/signup.component';
 import { AddProductComponent } from './modules/product/add-product/add-product.component';
 import { OverviewComponent } from './modules/dashboard/overview/overview.component';
+import { AdminProfileComponent } from './modules/users/admin/admin-profile/admin-profile.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -21,5 +22,9 @@ export const routes: Routes = [
   {
     path: 'overview',
     component: OverviewComponent,
+  },
+  {
+    path: 'profile',
+    component: AdminProfileComponent,
   },
 ];
